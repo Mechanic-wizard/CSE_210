613ad7b9fccaf07b7fe5262b18fdfd2a2096facf
Mechanic-wizard <padencbradley@gmail.com>
2026-09-16 10:49:25 -0600

    finished learning 1

---
+++ prepare/Learning01/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+    }
+}
+
+class Program : NewBaseType
+{
+}
